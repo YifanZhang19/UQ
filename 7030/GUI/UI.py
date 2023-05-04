5765364a24232d7de0dd457364c556d681168501
yifan zhang <yifan.zhang@sayweee.com>
2023-05-04 15:35:01 +1000

    some change

--- 7030/GUI/UI.py
+++ 7030/GUI/UI.py
@@ -88,18 +88,32 @@
 # window.mainloop()
 
 
-import tkinter as tk
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# frame1 = tk.Frame(master=window, width=100, height=100, bg="red")
+# frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+#
+# frame2 = tk.Frame(master=window, width=50, height=50, bg="yellow")
+# frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+#
+# frame3 = tk.Frame(master=window, width=25, height=25, bg="blue")
+# frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+#
+# window.mainloop()
 
-window = tk.Tk()
-
-frame1 = tk.Frame(master=window, width=100, height=100, bg="red")
-frame1.pack(fill=tk.X)
-
-frame2 = tk.Frame(master=window, width=50, height=50, bg="yellow")
-frame2.pack(fill=tk.X)
-
-frame3 = tk.Frame(master=window, width=25, height=25, bg="blue")
-frame3.pack(fill=tk.X)
-
-window.mainloop()
-
+# import tkinter as tk
+#
+# window = tk.Tk()
+#
+# frame = tk.Frame(master=window, width=150, height=150)
+# frame.pack()
+#
+# label1 = tk.Label(master=frame, text="im at (0, 0)", bg="red")
+# label1.place(x=0, y=0)
+#
+# label2 = tk.Label(master=frame, text="im at (75, 75)", bg="yellow")
+# label2.place(x=75, y=75)
+#
+# window.mainloop()
